286e45dfbc4deb38ad1c4f45aa4a73224e587fe2
Pierre Plagnes <pierre.plagnes@gmail.com>
2020-11-29 22:09:38 +0100

    remove bib name

--- src/main/java/com/xxx/f4m/streams/MergeTopics.java
+++ src/main/java/com/xxx/f4m/streams/MergeTopics.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.michelin.f4m.streams;
+package com.xxx.f4m.streams;
 
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.Serdes;
